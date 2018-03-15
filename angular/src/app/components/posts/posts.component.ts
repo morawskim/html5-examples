685ff9b7e6eb0e1436706293496e2631456550ae
Marcin Morawski <marcin@morawskim.pl>
2018-03-15 19:08:20 +0100

    Create PostModel

--- angular/src/app/components/posts/posts.component.ts
+++ angular/src/app/components/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service'
+import { PostModel } from '../../models/post.model'
 
 @Component({
   selector: 'app-posts',
@@ -7,11 +8,14 @@ import { PostService } from '../../services/post.service'
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts: any[] = [];
+  posts: PostModel[] = [];
   constructor(private service: PostService) { }
 
   ngOnInit() {
-    this.service.posts().subscribe(res => this.posts = res.json());
+    this.service.posts().subscribe((res) => {
+        let posts = res.json();
+        this.posts = posts.map(item => Object.assign(new PostModel() , item));
+    });
   }
 
 }
