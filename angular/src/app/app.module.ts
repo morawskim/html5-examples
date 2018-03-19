4ccfc416200180dca2aa79aeba49295c5ec4c21c
Marcin Morawski <marcin@morawskim.pl>
2018-03-19 19:28:59 +0100

    Router for post details

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostService } from './services/post.service';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'post/:id', component: PostDetailsComponent},
+      { path: '**', component: PostsComponent}
+    ])
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
